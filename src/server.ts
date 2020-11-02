1cd8b05866cb4a93f17e5192589059bcf85e594c
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2020-11-02 14:58:22 -0300

    chore: add eslint and configure

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.post('/users', (request, response) => {
   const { name, email } = request.body;
 
   const users = {
     name,
-    email
-  }
+    email,
+  };
 
   return response.json(users);
 });
 
-
 app.listen(3333, () => {
-  console.log('***************')
-  console.log('Server starded')
-  console.log('***************')
+  console.log('***************');
+  console.log('Server starded');
+  console.log('***************');
 });
