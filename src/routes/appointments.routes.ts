3a1eff9d1f716b2b764f81bd2a790aed077001da
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2020-11-02 17:14:12 -0300

    feat: criado metodo para listar datas

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual, parse } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.listAllAppointments();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
