987f6d93b64c179541c9bc759c0fb91a910bbcbf
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2020-11-02 16:36:03 -0300

    feat: add funçao para criar agendamento

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,35 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
-import { startOfHour, parseISO } from 'date-fns';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface IAppointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: IAppointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'Este horário já está agendado' });
+  }
 
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
