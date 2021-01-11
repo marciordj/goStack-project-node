584a38e2251cef72325a0cb03a883d1c582bd4e8
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2021-01-11 11:20:15 -0300

    feat: add jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import Users from '../models/Users';
 
@@ -8,8 +9,13 @@ interface IRequest {
   password: string;
 }
 
+interface IResponse {
+  user: Users;
+  token: string;
+}
+
 class AuthenticateUserService {
-  public async execute({ email, password }: IRequest): Promise<{ user: Users }> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const userRepository = getRepository(Users);
 
     const user = await userRepository.findOne({ where: { email } });
@@ -24,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email or password combination');
     }
 
+    const token = sign({}, '20ab12233b458a9fbf453b7e100deff9', {
+      subject: user.id,
+      expiresIn: '1d',
+    }); // primeiro parametro é o payload, ele vai ficar criptografado mas não é seguro, qualquer um consegue criptografar, o segundo é uma chave secreta
+
     return {
       user,
+      token,
     };
   }
 }
